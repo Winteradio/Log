6c254a95d6e082ce8d55408230230e18a9fa6078
Winteradio <tot4766@hanyang.ac.kr>
2023-05-31 20:52:42 +0900

    [Title]Edit for multiple-operator system
    [Desc]
    [Type/Branch]
    [Auther/Data]

--- demofile/main.cpp
+++ demofile/main.cpp
@@ -3,10 +3,13 @@
 
 #ifdef _WIN32
 int CALLBACK WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd )
+{
+    Log::Info(" Windows ");
 #elif __linux__
 int main()
-#endif
 {
+    Log::Info(" Linux ");
+#endif
     Log::Info(" Hello World! ");
     Log::Warn(" This Log Status is Warn ");
     Log::Error(" This Log Status is Error ");
