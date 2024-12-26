49f9cb524dc300db861d9ba8c404da6e01c2690e
Winteradio <tot4766@hanyang.ac.kr>
2024-12-26 20:29:49 +0900

    [Title] ExtenrlProject 예제 코드 수정
    [Desc] ExternalProject 예저코드 동작 확인
    - 최신 master 브런치에서 LogProject가 정상 동작하는지 확인
    [Type/Branch]
    [Auther/Data]
    
    ## Type - fix / test / feature / refac / bug / study
    ## Branch -
    ## Auther
    ## Date
    ## Desc

--- externaldemofile/main.cpp
+++ externaldemofile/main.cpp
@@ -1,23 +1,36 @@
-#include <iostream>
 #include <LogProject/Log.h>
+#include <LogProject/Except.h>
 
 void Example()
 {
-    Log::Info(" Hello World! ");
-    //throw Except("Something went wrong! %d ", 999 );
+    LOGINFO(" Hello World! ");
+    LOGWARN(" Comming Exception");    
+    
+    while (true)
+    {
+        static int value = 0;
+        value++;
+        LOGINFO(" Count : %d", value);
 
-    std::string Path = TEST_PATH;
-    Log::Info(" %s ", Path.c_str() );
-    Log::Warn(" The Stack is not escaped!! ");
+        if (value > 50000)
+        {
+            break;
+        }
+    }
+
+    throw Except("Something went wrong! %d ", 999 );
+
+    LOGINFO(" After Exception");
 }
 
 #ifdef _WIN32
+    #include <windows.h>
     #pragma comment(linker, "/entry:WinMainCRTStartup")
     #pragma comment(linker, "/subsystem:console")
 
 int CALLBACK WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd )
 {
-    Log::Info(" Windows ");
+    LOGINFO(" Platform is Windows");
 #else
 int main()
 {
@@ -29,10 +42,16 @@ int main()
     }
     catch (const Except& e)
     {
-        Log::Error( e.what() );
+        LOGERROR(e.what());
     }
 
-    Log::Print();
+    Log::Handler::Print();
+
+    Log::Handler::Clear();
+    LOGINFO(" Resume Log ");
+    Log::Handler::Print();
+
+    system("pause");
 
     return 0;
 }
