b6c88bcf92000de2fd0002c89d3bd242ef6142de
Winteradio <tot4766@hanyang.ac.kr>
2025-10-09 14:08:29 +0900

    [Title] edit
    [Desc]
    [Type/Branch]
    [Auther/Data]
    
    ## Type - fix / test / feature / refac / bug / study
    ## Branch -
    ## Auther
    ## Date
    ## Desc

--- cmake/Library.cmake
+++ cmake/Library.cmake
@@ -3,11 +3,12 @@ message(STATUS "Set the options for the library")
 
 option(BUILD_SHARED_LIBRARY "Build a shared library instead of a static one" OFF)
 
+set(INCLUDE_DIR ${CMAKE_BINARY_DIR}/include CACHE PATH "Include files Path")
 set(LIB_DIR ${CMAKE_BINARY_DIR}/lib CACHE PATH "Library files Path")
 set(BIN_DIR ${CMAKE_BINARY_DIR}/bin CACHE PATH "Execute files Path")
 
 # ------ Set the options for the library ------- #
-function(library_options)
+function(create_library)
 	message(STATUS "# Create the library")
 	message(STATUS "# The Library Type : ${BUILD_SHARED_LIBRARY}")
 	message(STATUS "# The Binary Directory : ${LIB_DIR}")
@@ -23,6 +24,24 @@ function(library_options)
 	    RUNTIME DESTINATION ${BIN_DIR}
 	    ARCHIVE DESTINATION ${LIB_DIR}
 	    LIBRARY DESTINATION ${LIB_DIR}
+	)
+endfunction()
+
+# ------ Copy the public header files for the library ------- #
+function(copy_library_headers)
+	cmake_parse_arguments(
+		ARG # Options / Single Value / Multi Value
+		""	
+		""
+		"HEADER_FILES"
+		${ARGN})
+
+	if (NOT ARG_HEADER_FILES)
+		message(STATUS "Warning : No header files to copy")
+	endif()
+
+	install(TARGETS ${PROJECT_NAME}
+		FILE_SET ${ARG_HEADER_FILES} DESTINATION ${INCLUDE_DIR}/${PROJECT_NAME}
 	)
 endfunction()
 
