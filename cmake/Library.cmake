bc8a032738655397fa032daa86095981d3bd360c
Winteradio <tot4766@hanyang.ac.kr>
2025-10-09 14:03:00 +0900

    [Title] edit
    [Desc]
    [Type/Branch]
    [Auther/Data]
    
    ## Type - fix / test / feature / refac / bug / study
    ## Branch -
    ## Auther
    ## Date
    ## Desc

--- cmake/Library.cmake
+++ cmake/Library.cmake
@@ -3,7 +3,6 @@ message(STATUS "Set the options for the library")
 
 option(BUILD_SHARED_LIBRARY "Build a shared library instead of a static one" OFF)
 
-set(INCLUDE_DIR ${CMAKE_BINARY_DIR}/include CACHE PATH "Include files Path")
 set(LIB_DIR ${CMAKE_BINARY_DIR}/lib CACHE PATH "Library files Path")
 set(BIN_DIR ${CMAKE_BINARY_DIR}/bin CACHE PATH "Execute files Path")
 
@@ -13,7 +12,6 @@ function(library_options)
 	message(STATUS "# The Library Type : ${BUILD_SHARED_LIBRARY}")
 	message(STATUS "# The Binary Directory : ${LIB_DIR}")
 	message(STATUS "# The Library Directory : ${BIN_DIR}")
-	message(STATUS "# The Include Directory : ${INCLUDE_DIR}")
 
 	if (${BUILD_SHARED_LIBRARY} STREQUAL "SHARED")
 	    add_library(${PROJECT_NAME} SHARED)
@@ -25,7 +23,6 @@ function(library_options)
 	    RUNTIME DESTINATION ${BIN_DIR}
 	    ARCHIVE DESTINATION ${LIB_DIR}
 	    LIBRARY DESTINATION ${LIB_DIR}
-	    FILE_SET HEADERS DESTINATION ${INCLUDE_DIR}/${PROJECT_NAME}
 	)
 endfunction()
 
